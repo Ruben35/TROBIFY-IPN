2da8ca6d7a687cf4af8547295b185da6cc67a9bc
David Madrigal Buendía <madrigal.bd@gmail.com>
2021-04-29 20:37:02 -0500

    Avance de busqueda y correción de unitario

--- api/routes/inmuebles.js
+++ api/routes/inmuebles.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllInmuebles, inmueblesCliente, inmueblesAgencia, inmuebleUnitario } = require('../controllers/inmuebleController');
+const { getAllInmuebles, inmueblesCliente, inmueblesAgencia, inmuebleUnitario, getInmuebles } = require('../controllers/inmuebleController');
 const router = express.Router();
 
 
@@ -7,6 +7,6 @@ router.get('/inmuebles',getAllInmuebles);
 router.get('/inmueblesCliente/:correo',inmueblesCliente);
 router.get('/inmueblesAgencia/:correo',inmueblesAgencia);
 router.get('', inmuebleUnitario); // ?inmueble_id=1
-
-
+router.get('/:propietario/:texto/:page', getInmuebles); //* traer todo
+// ?precio_inf=1&precio_sup=2
 module.exports = router
