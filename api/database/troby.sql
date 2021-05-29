6ad7bcd9ff10ddf19a2b35098803c45d2f4cd07c
David Madrigal Buendía <madrigal.bd@gmail.com>
2021-05-29 17:21:30 -0500

    Inserciones de prueba (Hechas en endpoint's)

--- api/database/troby.sql
+++ api/database/troby.sql
@@ -101,7 +101,7 @@ ENGINE = InnoDB;
 -- Table `troby`.`tipo_transaccion`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `troby`.`tipo_transaccion` (
-  `idtipo_transaccion` INT NOT NULL,
+  `idtipo_transaccion` INT NOT NULL AUTO_INCREMENT,
   `transaccion` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idtipo_transaccion`))
 ENGINE = InnoDB;
