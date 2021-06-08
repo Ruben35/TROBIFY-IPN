f8db7732abd652cabc6cd578a8896979712b47ff
Ruben35 <ruben111416@gmail.com>
2021-06-08 15:40:07 -0500

    Implementing ImgZoom and ImgList on Inmueble Unit

--- web/pages/inmuebles/[idInmueble].js
+++ web/pages/inmuebles/[idInmueble].js
@@ -1,15 +1,46 @@
 import Head from 'next/head'
-import { Box, Container, Grid, Paper, Typography } from '@material-ui/core'
+import { Box, Container, Grid, Paper, Typography, GridList, GridListTile, GridListTileBar } from '@material-ui/core'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import ImageZoom from 'react-medium-image-zoom'
+import useMediaQuery from '../../utils/CustomHooks';
 
 const useStyles = makeStyles((theme) => ({
     imgPort: {
         width: "100%",
+        maxHeight: "350px",
         borderRadius: "10px",
         boxShadow: "4px 7px 13px rgba(0, 0, 0, 0.24)",
+        marginBottom: theme.spacing(2),
+        objectFit: "cover"
     },
+    zoomImage:{
+        borderRadius: "10px",
+        boxShadow: "4px 7px 13px rgba(0, 0, 0, 0.24)",
+        objectFit: "cover"
+    },
+    rootGridList: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    gridListImg: {
+        cursor: 'pointer',
+        objectFit: "cover",
+        filter: 'brightness(50%)',
+        transition: 'filter 0.5s',
+        "&:hover": {
+            filter: 'brightness(100%)',
+        }
+    }
 }));
 
 
@@ -17,6 +48,8 @@ export default function verInmueble(){
     const router = useRouter();
     const { idInmueble } = router.query;
     const classes = useStyles();
+
+    const isSmall = useMediaQuery("(max-width: 769px)");
 
     //States
     const [title, setTitle] = useState("");
@@ -37,14 +70,33 @@ export default function verInmueble(){
     const [tipoTransaccion, setTipoTransaccion] = useState(1);
     const [precio, setPrecio] = useState("");
 
+
+    const tileData =[
+        {
+            title:"img1",
+            img:"/img/landing/carrusel0.jpg"
+        },
+        {
+            title:"img2",
+            img:"/img/landing/carrusel1.jpg"
+        },
+        {
+            title:"img3",
+            img:"/img/landing/carrusel2.jpg"
+        },
+    ]
+
+    //AuxStates
+    const [actualTile, setActualTile] = useState(tileData[0]);
+
     return (
         <>
             <Head>
-                <title>Trobify: Ver Inmueble \"{title}\"</title>
+                <title>Trobify: Ver Inmueble "{title}"</title>
             </Head>
             <Box minHeight="64px"></Box>
             <Container maxWidth="lg">
-                <Box display="flex" justifyContent="center" textAlign="center" marginTop={1}>
+                <Box display="flex" justifyContent="center" textAlign="center" marginTop={1} marginBottom={1}>
                     <Typography variant="h3">Titulo del Inmueble</Typography>
                 </Box>
                 <Paper elevation={3}>
@@ -52,7 +104,29 @@ export default function verInmueble(){
                         <Grid container spacing={2}>
                             <Grid item sm={6} xs={12}>
                                 <Box width="100%" height="auto">
-                                    <img src="/img/landing/carrusel0.jpg" className={classes.imgPort}/>
+                                    <ImageZoom 
+                                      image={{
+                                          src: actualTile.img,
+                                          alt: actualTile.title,
+                                          className: classes.imgPort
+                                      }}
+                                      zoomImage={{
+                                          src: actualTile.img,
+                                          alt: actualTile.title,
+                                          className: classes.zoomImage
+                                      }}
+                                      zoomMargin={isSmall?40:100}
+                                      />
+                                    {/* Carrusel */}
+                                    <div className={classes.rootGridList}>
+                                        <GridList className={classes.gridList} cols={2.5}>
+                                            {tileData.map((tile, index) => (
+                                            <GridListTile key={tile.title} className={classes.gridListImg} onClick={()=>setActualTile(tileData[index])}>
+                                                <img src={tile.img} alt={tile.title} />
+                                            </GridListTile>
+                                            ))}
+                                        </GridList>
+                                    </div>
                                 </Box>
                             </Grid>
                             <Grid item>
